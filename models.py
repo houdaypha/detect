5c6d929d9ad4355481caca2e36f5df22b91f4d92
houdaypha <atou.sec@gmail.com>
2023-05-25 18:09:50 +0100

    faster rcnn validation

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
+from typing import Any, Optional
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch
 import torch.nn as nn
-from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN
 import pytorch_lightning as pl
+from pytorch_lightning.utilities.model_summary import ModelSummary
 from dataset import CustomData
-import pdb
+from collections import OrderedDict
+import utils
 
 # FasterRCNN Model
 
@@ -24,12 +28,17 @@ class TFasterRCNN(nn.Module):
     def load(self, path):
         self.model.load_state_dict(torch.load(path))
 
+    def info(self, verbose=False):
+        # TODO: Return a more verbose model info
+        print(self.model.__str__())
+
 
 class PLFasterRCNN(pl.LightningModule):
-    def __init__(self, model):
+    def __init__(self, model: FasterRCNN):
         super().__init__()
         self.model = model
         self.training_step_outputs = []
+        self.validation_step_outputs = []
 
     def training_step(self, batch, batch_idx):
         image, targets = batch
@@ -77,10 +86,66 @@ class PLFasterRCNN(pl.LightningModule):
         # free up the memory
         self.training_step_outputs.clear()
 
+    def validation_step(self, batch, batch_idx):
+        image, targets = batch
+        image, targets = list(image), list(targets)
+        loss_dict, _ = utils.eval_forward(self.model, image, targets)
+        loss_sum = sum(loss for loss in loss_dict.values())
+
+        # Validation loss dictionary
+        val_loss_dict = dict()
+        for k, v in loss_dict.items():
+            val_loss_dict[f'val_{k}'] = v
+        val_loss_dict['loss'] = loss_sum
+
+        self.log('val_loss', loss_sum, logger=False)
+        self.validation_step_outputs.append(val_loss_dict)
+        return val_loss_dict
+
+    def on_validation_epoch_end(self):
+        # Getting outputs
+        outputs = self.validation_step_outputs
+
+        # Logging losses
+        writer = self.logger.experiment
+        tloss = torch.stack([x["loss"] for x in outputs]).mean()
+        closs = torch.stack([x["val_loss_classifier"] for x in outputs]).mean()
+        rloss = torch.stack([x["val_loss_box_reg"] for x in outputs]).mean()
+        rpnloss = torch.stack(
+            [x["val_loss_rpn_box_reg"] for x in outputs]).mean()
+        oloss = torch.stack([x["val_loss_objectness"] for x in outputs]).mean()
+        writer.add_scalar(
+            'Validation/Total Loss',
+            tloss,
+            self.current_epoch)
+        writer.add_scalar(
+            'Validation/Classifier Loss',
+            closs,
+            self.current_epoch)
+        writer.add_scalar(
+            'Validation/Regressor loss',
+            rloss,
+            self.current_epoch)
+        writer.add_scalar(
+            'Validation/Region Proposal Network loss',
+            rpnloss,
+            self.current_epoch)
+        writer.add_scalar(
+            'Validation/Objectness loss',
+            oloss,
+            self.current_epoch)
+
+        # free up the memory
+        self.validation_step_outputs.clear()
+
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(
             self.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
         return optimizer
+
+    def info(self, verbose=False):
+        max_depth = -1 if verbose else 1
+        return ModelSummary(self, max_depth=max_depth)
 
 
 class Model:
@@ -148,16 +213,34 @@ class Model:
         elif self.type == 'yolo':
             raise NotImplementedError
 
-    def save(self, path):
+    def export(self, path):
         raise NotImplementedError
 
     def load(self, path):
         raise NotImplementedError
 
+    def predict(self, source):
+        """
+        Perform prediction using the model
+
+        Args:
+            source (str | int | PIL | np.ndarray): The source of the image to make predictions on.
+        """
+        raise NotImplementedError
+
+    def info(self, verbose=True):
+        """
+        Logs model info.
+
+        Args:
+            verbose (bool): Controls verbosity.
+        """
+        self.model.info(verbose=verbose)
+
 
 def main():
     model = Model('fasterrcnn', 2, 'gpu')
-    model.train('./conf/torch.yaml', epochs=100, batch=2, workers=4)
+    model.train('./conf/torch.yaml', epochs=10, batch=2, workers=4)
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,225 @@
+from PIL import ImageDraw, ImageFont
+from torchvision import transforms
+import torch
+import warnings
+from collections import OrderedDict
+from torchvision.models.detection.roi_heads import fastrcnn_loss
+from torchvision.models.detection.rpn import concat_box_prediction_layers
+
+COLORS = (
+    "#FF3838",
+    "#2C99A8",
+    "#FF701F",
+    "#6473FF",
+    "#CFD231",
+    "#48F90A",
+    "#92CC17",
+    "#3DDB86",
+    "#1A9334",
+    "#00D4BB",
+    "#FF9D97",
+    "#00C2FF",
+    "#344593",
+    "#FFB21D",
+    "#0018EC",
+    "#8438FF",
+    "#520085",
+    "#CB38FF",
+    "#FF95C8",
+    "#FF37C7",
+)
+
+
+def yolo_draw_predections(image, object_predictions):
+    # Convert PIL image to ImageDraw object
+    draw = ImageDraw.Draw(image)
+
+    prediction = object_predictions[0]
+
+    scores = prediction['scores'].cpu().detach().numpy()
+
+    if scores.size == 0:
+        return image
+
+    for idx, score in enumerate(scores):
+        # Extract prediction details
+        bbox = prediction['boxes'][idx].cpu().detach().numpy()
+        # score = prediction['scores'][0].cpu().detach().numpy()
+        category_id = prediction['labels'][idx].cpu().detach().numpy()
+        category_name = f'name_{category_id}'
+
+        color = COLORS[category_id.item()]
+
+        # Define the font properties for text overlay
+        font = ImageFont.truetype("Montserrat-Bold.ttf", size=18)
+
+        # Draw the bounding box rectangle
+        print(f'{score}: {bbox}')
+        draw.rectangle(bbox, outline=color, width=3)
+
+        # Prepare the text overlay
+        text = f"{category_name} ({score:.2f})"
+
+        # Calculate the position for text overlay
+        text_width, text_height = draw.textsize(text, font=font)  # font = font
+        x = bbox[0]
+        y = bbox[1] - text_height
+
+        # Draw the text overlay
+        draw.rectangle((x, y, x + text_width, y + text_height), fill=color)
+        draw.text((x, y), text, fill="white", font=font)  # font = font
+
+    return image
+
+
+def draw_predections(image, boxes, labels, scores, thr):
+    # Convert PIL image to ImageDraw object
+    image = transforms.ToPILImage()(image)
+    draw = ImageDraw.Draw(image)
+    width = 3
+
+    for boxe, label, score in zip(boxes, labels, scores):
+        if score > thr:
+            # Extract prediction details
+            bbox = boxe.cpu().detach().numpy()
+            # score = prediction['scores'][0].cpu().detach().numpy()
+            category_id = label.cpu().detach().numpy()
+            category_name = f'name_{category_id}'
+
+            color = COLORS[category_id.item()]
+
+            # Define the font properties for text overlay
+            font = ImageFont.truetype("Montserrat-Bold.ttf", size=18)
+
+            # Draw the bounding box rectangle
+            draw.rectangle(tuple(bbox), outline=color, width=width)
+
+            # Prepare the text overlay
+            text = f"{category_name} ({score:.2f})"
+
+            # Calculate the position for text overlay
+            # text_width, text_height = draw.textsize(text, font=font) # font =
+            # font
+            x = bbox[0]
+            y = bbox[1]
+
+            l, t, r, b = draw.textbbox((x, y), text, font=font)
+            h = b - t + width
+            # Draw the text overlay
+            draw.rectangle((l, t - h, r, b - h), fill=color)
+            draw.text((x, y - h), text, fill="white", font=font)  # font = font
+
+    return image
+
+
+def eval_forward(model, images, targets):
+    """
+    Args:
+        images (list[Tensor]): images to be processed
+        targets (list[Dict[str, Tensor]]): ground-truth boxes present in the image (optional)
+    Returns:
+        result (list[BoxList] or dict[Tensor]): the output from the model.
+            It returns list[BoxList] contains additional fields
+            like `scores`, `labels` and `mask` (for Mask R-CNN models).
+    """
+    if model.training:
+        model.eval()
+
+    original_image_sizes = []
+    for img in images:
+        val = img.shape[-2:]
+        assert len(val) == 2
+        original_image_sizes.append((val[0], val[1]))
+
+    images, targets = model.transform(images, targets)
+
+    # Check for degenerate boxes
+    # TODO: Move this to a function
+    if targets is not None:
+        for target_idx, target in enumerate(targets):
+            boxes = target["boxes"]
+            degenerate_boxes = boxes[:, 2:] <= boxes[:, :2]
+            if degenerate_boxes.any():
+                # print the first degenerate box
+                bb_idx = torch.where(degenerate_boxes.any(dim=1))[0][0]
+                degen_bb = boxes[bb_idx].tolist()
+                raise ValueError(
+                    "All bounding boxes should have positive height and width."
+                    f" Found invalid box {degen_bb} for target at index {target_idx}.")
+
+    features = model.backbone(images.tensors)
+    if isinstance(features, torch.Tensor):
+        features = OrderedDict([("0", features)])
+    model.rpn.training = True
+    # model.roi_heads.training=True
+
+    # proposals, proposal_losses = model.rpn(images, features, targets)
+    features_rpn = list(features.values())
+    objectness, pred_bbox_deltas = model.rpn.head(features_rpn)
+    anchors = model.rpn.anchor_generator(images, features_rpn)
+
+    num_images = len(anchors)
+    num_anchors_per_level_shape_tensors = [o[0].shape for o in objectness]
+    num_anchors_per_level = [s[0] * s[1] * s[2]
+                             for s in num_anchors_per_level_shape_tensors]
+    objectness, pred_bbox_deltas = concat_box_prediction_layers(
+        objectness, pred_bbox_deltas)
+    # apply pred_bbox_deltas to anchors to obtain the decoded proposals
+    # note that we detach the deltas because Faster R-CNN do not backprop through
+    # the proposals
+    proposals = model.rpn.box_coder.decode(pred_bbox_deltas.detach(), anchors)
+    proposals = proposals.view(num_images, -1, 4)
+    proposals, scores = model.rpn.filter_proposals(
+        proposals, objectness, images.image_sizes, num_anchors_per_level)
+
+    proposal_losses = {}
+    assert targets is not None
+    labels, matched_gt_boxes = model.rpn.assign_targets_to_anchors(
+        anchors, targets)
+    regression_targets = model.rpn.box_coder.encode(matched_gt_boxes, anchors)
+    loss_objectness, loss_rpn_box_reg = model.rpn.compute_loss(
+        objectness, pred_bbox_deltas, labels, regression_targets
+    )
+    proposal_losses = {
+        "loss_objectness": loss_objectness,
+        "loss_rpn_box_reg": loss_rpn_box_reg,
+    }
+
+    # detections, detector_losses = model.roi_heads(features, proposals, images.image_sizes, targets)
+    image_shapes = images.image_sizes
+    proposals, matched_idxs, labels, regression_targets = model.roi_heads.select_training_samples(
+        proposals, targets)
+    box_features = model.roi_heads.box_roi_pool(
+        features, proposals, image_shapes)
+    box_features = model.roi_heads.box_head(box_features)
+    class_logits, box_regression = model.roi_heads.box_predictor(box_features)
+
+    result = []
+    detector_losses = {}
+    loss_classifier, loss_box_reg = fastrcnn_loss(
+        class_logits, box_regression, labels, regression_targets)
+    detector_losses = {
+        "loss_classifier": loss_classifier,
+        "loss_box_reg": loss_box_reg}
+    boxes, scores, labels = model.roi_heads.postprocess_detections(
+        class_logits, box_regression, proposals, image_shapes)
+    num_images = len(boxes)
+    for i in range(num_images):
+        result.append(
+            {
+                "boxes": boxes[i],
+                "labels": labels[i],
+                "scores": scores[i],
+            }
+        )
+    detections = result
+    detections = model.transform.postprocess(
+        detections,
+        images.image_sizes,
+        original_image_sizes)  # type: ignore[operator]
+    model.rpn.training = False
+    model.roi_heads.training = False
+    losses = {}
+    losses.update(detector_losses)
+    losses.update(proposal_losses)
+    return losses, detections
